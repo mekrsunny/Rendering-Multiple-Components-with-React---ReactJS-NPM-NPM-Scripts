905ba512ce895305470cef2c9ef9db1d036f5001
kumar sunny <coolsunny2099@gmail.com>
2022-11-27 20:33:11 +0530

    React Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,10 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+    <h1 data-ns-test="project-name">Anime Watching Platform</h1>
+      <p data-ns-test= "project-description">As I'm a anime fan i face a lots of issue while watching anime so, i want to create a platform where a anime fan can watch anime without having ad.</p>
+    </div>
   )
 }
 
